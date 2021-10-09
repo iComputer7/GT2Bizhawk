5d5f2c7c7adf4233e382a1212474215ee72f81c2
Sean <6298231+iComputer7@users.noreply.github.com>
2021-10-09 18:10:38 -0500

    Add project files.

---
+++ Main.Designer.cs
@@ -0,0 +1,202 @@
+﻿
+namespace GT2Bizhawk {
+    partial class MainForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.Windows.Forms.ListViewItem listViewItem5 = new System.Windows.Forms.ListViewItem(new string[] {
+            "Money",
+            "0"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem6 = new System.Windows.Forms.ListViewItem(new string[] {
+            "Days",
+            "0"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem7 = new System.Windows.Forms.ListViewItem(new string[] {
+            "Current Car Index",
+            "0"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem8 = new System.Windows.Forms.ListViewItem(new string[] {
+            "Garage Size",
+            "0"}, -1);
+            this.DetectedGameLbl = new System.Windows.Forms.Label();
+            this.MainTabControl = new System.Windows.Forms.TabControl();
+            this.GameTab = new System.Windows.Forms.TabPage();
+            this.GotoCar1Btn = new System.Windows.Forms.Button();
+            this.MaxMoneyBtn = new System.Windows.Forms.Button();
+            this.GameListView = new System.Windows.Forms.ListView();
+            this.PropNameHead = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ValueHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.GarageTab = new System.Windows.Forms.TabPage();
+            this.GetInBtn = new System.Windows.Forms.Button();
+            this.GarageListView = new System.Windows.Forms.ListView();
+            this.MainTabControl.SuspendLayout();
+            this.GameTab.SuspendLayout();
+            this.GarageTab.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // DetectedGameLbl
+            // 
+            this.DetectedGameLbl.AutoSize = true;
+            this.DetectedGameLbl.Dock = System.Windows.Forms.DockStyle.Top;
+            this.DetectedGameLbl.Location = new System.Drawing.Point(0, 0);
+            this.DetectedGameLbl.Name = "DetectedGameLbl";
+            this.DetectedGameLbl.Padding = new System.Windows.Forms.Padding(5);
+            this.DetectedGameLbl.Size = new System.Drawing.Size(118, 23);
+            this.DetectedGameLbl.TabIndex = 1;
+            this.DetectedGameLbl.Text = "Detected Game: N/A";
+            // 
+            // MainTabControl
+            // 
+            this.MainTabControl.Controls.Add(this.GameTab);
+            this.MainTabControl.Controls.Add(this.GarageTab);
+            this.MainTabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainTabControl.Location = new System.Drawing.Point(0, 23);
+            this.MainTabControl.Name = "MainTabControl";
+            this.MainTabControl.SelectedIndex = 0;
+            this.MainTabControl.Size = new System.Drawing.Size(400, 427);
+            this.MainTabControl.TabIndex = 2;
+            this.MainTabControl.SelectedIndexChanged += new System.EventHandler(this.MainTabControl_SelectedIndexChanged);
+            // 
+            // GameTab
+            // 
+            this.GameTab.Controls.Add(this.GotoCar1Btn);
+            this.GameTab.Controls.Add(this.MaxMoneyBtn);
+            this.GameTab.Controls.Add(this.GameListView);
+            this.GameTab.Location = new System.Drawing.Point(4, 22);
+            this.GameTab.Name = "GameTab";
+            this.GameTab.Padding = new System.Windows.Forms.Padding(3);
+            this.GameTab.Size = new System.Drawing.Size(392, 401);
+            this.GameTab.TabIndex = 0;
+            this.GameTab.Text = "Game";
+            this.GameTab.UseVisualStyleBackColor = true;
+            // 
+            // GotoCar1Btn
+            // 
+            this.GotoCar1Btn.Location = new System.Drawing.Point(89, 372);
+            this.GotoCar1Btn.Name = "GotoCar1Btn";
+            this.GotoCar1Btn.Size = new System.Drawing.Size(75, 23);
+            this.GotoCar1Btn.TabIndex = 2;
+            this.GotoCar1Btn.Text = "Get In Car 0";
+            this.GotoCar1Btn.UseVisualStyleBackColor = true;
+            this.GotoCar1Btn.Click += new System.EventHandler(this.GotoCar1Btn_Click);
+            // 
+            // MaxMoneyBtn
+            // 
+            this.MaxMoneyBtn.Location = new System.Drawing.Point(8, 372);
+            this.MaxMoneyBtn.Name = "MaxMoneyBtn";
+            this.MaxMoneyBtn.Size = new System.Drawing.Size(75, 23);
+            this.MaxMoneyBtn.TabIndex = 1;
+            this.MaxMoneyBtn.Text = "Max Money";
+            this.MaxMoneyBtn.UseVisualStyleBackColor = true;
+            this.MaxMoneyBtn.Click += new System.EventHandler(this.MaxMoneyBtn_Click);
+            // 
+            // GameListView
+            // 
+            this.GameListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.PropNameHead,
+            this.ValueHeader});
+            this.GameListView.Dock = System.Windows.Forms.DockStyle.Top;
+            this.GameListView.HideSelection = false;
+            this.GameListView.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem5,
+            listViewItem6,
+            listViewItem7,
+            listViewItem8});
+            this.GameListView.Location = new System.Drawing.Point(3, 3);
+            this.GameListView.Name = "GameListView";
+            this.GameListView.Size = new System.Drawing.Size(386, 363);
+            this.GameListView.TabIndex = 0;
+            this.GameListView.UseCompatibleStateImageBehavior = false;
+            this.GameListView.View = System.Windows.Forms.View.Details;
+            // 
+            // PropNameHead
+            // 
+            this.PropNameHead.Text = "Property";
+            this.PropNameHead.Width = 100;
+            // 
+            // ValueHeader
+            // 
+            this.ValueHeader.Text = "Value";
+            this.ValueHeader.Width = 280;
+            // 
+            // GarageTab
+            // 
+            this.GarageTab.Controls.Add(this.GetInBtn);
+            this.GarageTab.Controls.Add(this.GarageListView);
+            this.GarageTab.Location = new System.Drawing.Point(4, 22);
+            this.GarageTab.Name = "GarageTab";
+            this.GarageTab.Padding = new System.Windows.Forms.Padding(3);
+            this.GarageTab.Size = new System.Drawing.Size(392, 401);
+            this.GarageTab.TabIndex = 1;
+            this.GarageTab.Text = "Garage";
+            this.GarageTab.UseVisualStyleBackColor = true;
+            // 
+            // GetInBtn
+            // 
+            this.GetInBtn.Location = new System.Drawing.Point(309, 372);
+            this.GetInBtn.Name = "GetInBtn";
+            this.GetInBtn.Size = new System.Drawing.Size(75, 23);
+            this.GetInBtn.TabIndex = 1;
+            this.GetInBtn.Text = "Get In";
+            this.GetInBtn.UseVisualStyleBackColor = true;
+            // 
+            // GarageListView
+            // 
+            this.GarageListView.Dock = System.Windows.Forms.DockStyle.Top;
+            this.GarageListView.HideSelection = false;
+            this.GarageListView.Location = new System.Drawing.Point(3, 3);
+            this.GarageListView.Name = "GarageListView";
+            this.GarageListView.Size = new System.Drawing.Size(386, 363);
+            this.GarageListView.TabIndex = 0;
+            this.GarageListView.UseCompatibleStateImageBehavior = false;
+            this.GarageListView.View = System.Windows.Forms.View.List;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 450);
+            this.Controls.Add(this.MainTabControl);
+            this.Controls.Add(this.DetectedGameLbl);
+            this.Name = "MainForm";
+            this.MainTabControl.ResumeLayout(false);
+            this.GameTab.ResumeLayout(false);
+            this.GarageTab.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label DetectedGameLbl;
+        private System.Windows.Forms.TabControl MainTabControl;
+        private System.Windows.Forms.TabPage GameTab;
+        private System.Windows.Forms.ListView GameListView;
+        private System.Windows.Forms.ColumnHeader PropNameHead;
+        private System.Windows.Forms.ColumnHeader ValueHeader;
+        private System.Windows.Forms.TabPage GarageTab;
+        private System.Windows.Forms.Button MaxMoneyBtn;
+        private System.Windows.Forms.Button GotoCar1Btn;
+        private System.Windows.Forms.Button GetInBtn;
+        private System.Windows.Forms.ListView GarageListView;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,102 @@
+﻿using BizHawk.Client.Common;
+using BizHawk.Client.EmuHawk;
+using System.Windows.Forms;
+
+namespace GT2Bizhawk {
+	[ExternalTool("GT2HybridTool", Description = "GT2 Hybrid Tool")]
+	public sealed partial class MainForm : ToolFormBase, IExternalToolForm {
+        protected override string WindowTitleStatic => "GT2 Hybrid Tool";
+		[RequiredApi]
+		public ApiContainer PubApis { get; set; }
+		private ApiContainer Api => PubApis!;
+		private bool IsGameLoaded => Api.GameInfo.GetRomName() != "Null";
+		private uint LastGarageCount = 0;
+
+		public MainForm() {
+			//winforms stuff
+			InitializeComponent();
+        }
+
+		private void GarageTabUpdate(bool force = false) {
+			uint curGarageCount = Api.Memory.ReadU32(0x1cd554);
+			if (LastGarageCount == curGarageCount && !force)
+				return;
+
+			GarageListView.BeginUpdate();
+			GarageListView.Items.Clear();
+			//cars are 0xa4 bytes long
+			for (uint i = 0; i < (curGarageCount * 0xa4); i += 0xa4) {
+				uint carStart = 0x1cd558 + i;
+				uint carCode = Api.Memory.ReadU32(carStart);
+				GarageListView.Items.Add($"{carCode:X8}");
+            }
+			GarageListView.EndUpdate();
+			LastGarageCount = curGarageCount;
+		}
+        
+		public override void Restart() {
+			string? romName = Api.GameInfo.GetRomName();
+			if (romName != "Null") {
+				DetectedGameLbl.Text = "Detected Game: " + romName;
+            }
+		}
+
+		protected override void UpdateAfter() {
+			if (!IsGameLoaded)
+				return;
+
+			switch (MainTabControl.SelectedIndex) {
+				//Game tab
+				case 0: {
+					GameListView.Items[0].SubItems[1].Text = $"{Api.Memory.ReadU32(0x1d1568):N0}"; //Money
+					GameListView.Items[1].SubItems[1].Text = $"{Api.Memory.ReadS32(0x1c99d8):N0}"; //Days
+
+					//Current Car
+					uint currentCar = Api.Memory.ReadU32(0x1d156c);
+					if (currentCar == 0xffff)
+						GameListView.Items[2].SubItems[1].Text = "N/A";
+					else
+						GameListView.Items[2].SubItems[1].Text = $"{currentCar}";
+					
+					GameListView.Items[3].SubItems[1].Text = $"{Api.Memory.ReadU32(0x1cd554):N0}"; //Garage Size
+
+					break;
+				}
+
+				//Garage tab
+				case 1: {
+					GarageTabUpdate();
+					break;
+                }
+				
+				default: {
+					return;
+                }
+			}
+		}
+
+        private void MaxMoneyBtn_Click(object sender, System.EventArgs e) {
+			if (IsGameLoaded)
+				Api.Memory.WriteU32(0x1d1568, 99999999);
+		}
+
+        private void GotoCar1Btn_Click(object sender, System.EventArgs e) {
+			if (IsGameLoaded)
+				Api.Memory.WriteU32(0x1d156c, 0);
+		}
+
+        private void MainTabControl_SelectedIndexChanged(object sender, System.EventArgs e) {
+			if (IsGameLoaded)
+				switch (MainTabControl.SelectedIndex) {
+					case 1: {
+						GarageTabUpdate();
+						break;
+					}
+
+					default: {
+						return;
+					}
+				}
+		}
+    }
+}

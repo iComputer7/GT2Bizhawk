7007f72265ad21edab1a1de749e1d5ec328095b2
Sean <6298231+iComputer7@users.noreply.github.com>
2021-10-11 21:44:34 -0500

    basis of configuration

---
+++ GameVersions/BaseGameVersion.cs
@@ -0,0 +1,10 @@
+﻿namespace GT2Bizhawk.GameVersions {
+    public abstract class BaseGameVersion {
+        public static uint GarageCount { get; }
+        public static uint GarageStart { get; }
+        public static uint Money { get; }
+        public static uint Days { get; }
+        public static uint CurrentCar { get; }
+        public static uint GarageSize { get; }
+    }
+}
---
+++ GameVersions/NtscU12.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GT2Bizhawk.GameVersions {
+    public class NtscU12 {
+        public static uint GarageCount => 0x1cd554;
+        public static uint GarageStart => 0x1cd558;
+        public static uint Money => 0x1d1568;
+        public static uint Days => 0x1c99d8;
+        public static uint CurrentCar => 0x1d156c;
+        public static uint GarageSize => 0x1cd554;
+    }
+}

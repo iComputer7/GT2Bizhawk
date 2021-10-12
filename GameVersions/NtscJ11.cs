c1c03e4468cf0feef437c59c4b78af0118777aed
Sean <6298231+iComputer7@users.noreply.github.com>
2021-10-11 22:48:30 -0500

    Starting on JP 1.1 addresses

--- GameVersions/NtscJ11.cs
+++ GameVersions/NtscJ11.cs
@@ -1,11 +1,11 @@
 ﻿namespace GT2Bizhawk.GameVersions {
     public class NtscJ11 : IBaseGameVersion {
         public string GameHash => "B5A363A3";
-        public uint GarageCount => 0x1cd554;
-        public uint GarageStart => 0x1cd558;
-        public uint Money => 0x1d1568;
-        public uint Days => 0x1c99d8;
-        public uint CurrentCar => 0x1d156c;
-        public uint GarageSize => 0x1cd554;
+        public uint GarageCount => 0;
+        public uint GarageStart => 0;
+        public uint Money => 0x1d13c8;
+        public uint Days => 0;
+        public uint CurrentCar => 0;
+        public uint GarageSize => 0;
     }
 }
